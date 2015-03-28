4fdd26edd34b34dd2029f3de84765470954c98c9
Yannick Jost <yannick@yjost.com>
2015-03-28 16:14:35 +0100

    Fix webconfig

--- brewery/webconfig.py
+++ brewery/webconfig.py
@@ -9,29 +9,11 @@ import string
 def gethostname():
     return socket.gethostname()
 
-def setup_nginx_config_save():
-    host_config_root = os.path.join(os.path.dirname(__file__), "web")
-    host_config_filename = "nginx-custom-{}.conf".format(gethostname())
-    host_config_file = os.path.join(host_config_root, host_config_filename)
-    host_config_file_default = "nginx-custom-default.conf"
-    host_config_file_target = os.path.join(host_config_root, "nginx-custom.conf")
-
-    logger.log("Host config file: {}".format(host_config_file))
-
-    if not os.path.exists(host_config_file):
-        logger.log("Host config file not found using default: {}".format(host_config_file_default))
-        host_config_file=host_config_file_default
-
-    try:
-        shutil.copy(host_config_file, host_config_file_target)
-    except Exception as e:
-        logger.exception(e)
-
 def setup_nginx_config():
     host_config_root = os.path.join(os.path.dirname(__file__), "web")
     host_config_filename = "nginx-config-{}.json".format(gethostname())
     host_config_file = os.path.join(host_config_root, host_config_filename)
-    host_config_file_default = "nginx-config-default.json"
+    host_config_file_default = "web/nginx-config-default.json"
     host_config_file_template = os.path.join(host_config_root, "nginx.conf.template")
     host_config_file_target = os.path.join(host_config_root, "nginx.conf")
 
