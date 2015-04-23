030916cfee94ae8e17c4c8886cc292ad67fe7a37
Éric ALBER <eric.alber@gmail.com>
2015-04-24 00:33:51 +0200

    Add a remote update & run tool

---
+++ tools/remotebrewery.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--push", action = "store_true", help = "push the local source code with rsync")
+parser.add_argument("-r", "--run", action = "store_true", help = "execute the remote brewery")
+parser.add_argument("remote", help = "IP or hostname of the robot")
+args = parser.parse_args()
+
+if args.push:
+    bhware = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    subprocess.call(["rsync",
+                     "--archive",
+                     "--verbose",
+                     "--checksum",
+                     "--exclude-from", os.path.join(bhware, ".hgignore"),
+                     "brewery",
+                     "root@{}:/root/bhware".format(args.remote)], cwd = bhware)
+
+if args.run:
+    os.execl("/usr/bin/ssh",
+             "-t",
+             "root@{}".format(args.remote),
+             "source /root/.profile && /root/bhware/brewery/brewery.py")
