bc213504b9fbaf9bda5493d34d41726b587a4bcc
Éric ALBER <eric.alber@gmail.com>
2015-05-07 23:02:22 +0200

    Add a simple tool to play a media on the smartphone

---
+++ tools/playmedia.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import struct
+import socket
+import sys
+
+if len(sys.argv) < 5:
+    print("Usage: {} <server> <port> <say|play> <text|path>".format(sys.argv[0]))
+    sys.exit(1)
+
+sock = socket.create_connection((sys.argv[1], int(sys.argv[2])))
+
+if sys.argv[3] == 'play':
+    msg_type = 250
+else:
+    msg_type = 251
+
+msg = " ".join(sys.argv[4:])
+data = struct.pack("<BB254s", msg_type, 254, bytes(msg,'utf-8','ignore'))
+sock.send(data)
+sock.close()
+
