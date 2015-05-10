b1fee16bb8c2b323a7448bf3aae3619f8d946d85
Éric ALBER <eric.alber@gmail.com>
2015-05-10 15:26:29 +0200

    Keep the bulb grabber elevator up to prevent the ball to fall

--- brewery/statemachines/bulbgrabber.py
+++ brewery/statemachines/bulbgrabber.py
@@ -25,6 +25,7 @@ class Main(State):
 
     def on_start(self, packet):
         if packet.value == 0:
+             self.yield_at(90000, EndOfMatch())
              yield Trigger(LIGHTER_ELEVATOR_BULB)
              yield Timer(200)
              yield Trigger(LIGHTER_GRIPPER_CLOSE)
@@ -36,10 +37,22 @@ class Main(State):
              yield Trigger(LIGHTER_ELEVATOR_UP)
              yield Trigger(LIGHTER_GRIPPER_OPEN)
              yield Timer(500)
-             yield Trigger(LIGHTER_ELEVATOR_DOWN)
-             yield ServoTorqueControl([LIGHTER_ELEVATOR_ID, LIGHTER_GRIPPER_ID], False)
              bulb_presence = yield GetInputStatus(MAIN_INPUT_LEFT_BULB_PRESENCE)
              self.robot.has_left_bulb = bulb_presence == 0
              bulb_presence = yield GetInputStatus(MAIN_INPUT_RIGHT_BULB_PRESENCE)
              self.robot.has_right_bulb = bulb_presence == 0
              yield None
+
+
+
+
+##################################################
+# End of match
+
+
+
+
+class EndOfMatch(statemachine.State):
+
+    def on_enter(self):
+         yield ServoTorqueControl([LIGHTER_ELEVATOR_ID, LIGHTER_GRIPPER_ID], False)
