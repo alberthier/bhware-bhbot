4647e385f4541bc09c34e2bec3641eeafd22075f
Yannick Jost <yannick@yjost.com>
2015-05-13 10:35:54 +0200

    Well we misunderstood the rules...

--- brewery/goaldecider.py
+++ brewery/goaldecider.py
@@ -285,17 +285,15 @@ class Explorer:
             if "BUILD" in last.tags :
                 if last.builder_action[0]<0 and new_world.left_builder_count:
                     if new_world.has_left_bulb:
-                        action_score+=new_world.left_builder_count*2
-                        action_score+=3
+                        action_score+=new_world.left_builder_count*3
                     else:
-                        action_score+=1
+                        action_score+=new_world.left_builder_count*2
 
                 if last.builder_action[1]<0 and new_world.right_builder_count:
                     if new_world.has_right_bulb:
-                        action_score+=new_world.right_builder_count*2
-                        action_score+=3
+                        action_score+=new_world.right_builder_count*3
                     else:
-                        action_score+=1
+                        action_score+=new_world.right_builder_count*2
 
                 if not new_world.left_builder_count and not new_world.right_builder_count:
                     raise UnneededAction(last, "No stand to deposit")
