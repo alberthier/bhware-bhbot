500863f0d28f05513d911cafd0b3e08025f38b4f
Éric ALBER <eric.alber@gmail.com>
2017-05-19 13:02:34 +0200

    FIx move

--- brewery/statemachines/main.py
+++ brewery/statemachines/main.py
@@ -539,7 +539,7 @@ class StaticStrategy(State):
         yield StartArmSequence('ClearFirstModule')
 
         # Deplacement vers fusee polychrome bleu
-        yield MoveLineTo(0.927 - 0.02, 0.923 + 0.02)
+        yield MoveLineTo(0.927 - 0.05, 0.923 + 0.05)
         yield StartArmSequence('InitArm')
         yield RotateTo(-math.pi/4.0)
         yield MoveLineTo(1.25, 0.6)
